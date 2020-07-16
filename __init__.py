a98af2e460c63e340464388544f5c1c31176615b
SamHarris2020 <sam.career@outlook.com>
2020-07-15 23:02:03 -0700

    Testing MycroftSkill.Location Format

--- __init__.py
+++ __init__.py
@@ -1,32 +1,46 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
-from datetime import datetime, timedelta
 import json
 import requests
 import os
 import time
+import datetime
 
 """
     This skill uses the AirNow (https://docs.airnowapi.org/) 
     Specifically, The Lat/Lon API (https://docs.airnowapi.org/forecastsbylatlon/docs)
 """
 
+# Request URL
 URL = "http://www.airnowapi.org/aq/forecast/latLong/?format=application/json"
-api_key = "19C1A418-EFC9-4FC5-A58C-9E62C29DBBE9" # REMOVE AFTER TESTING
-distance = 25  # How far the from the lat/long airnow will check
-last_request = None
 
 
 class AirQualityIndex(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.api_key = self.settings.get('show_time')
+        self.lat = 0.0
+        self.lon = 0.0
+
+        self.log.debug(MycroftSkill.location)
+        self.log.debug(type(MycroftSkill.location))
+        self.log.debug(MycroftSkill.location_pretty)
+        self.log.debug(type(MycroftSkill.location_pretty))
 
     @intent_handler('index.quality.air.intent')
     def handle_index_quality_air(self, message):
-        self.speak_dialog('index.quality.air')
+
+        query = self.get_air_quality()
+        if query is not None:
+            ozone = query[0]["Category"]["Name"]
+            particulates = query[1]["Category"]["Name"]
+            self.speak_dialog("particulates are {0} and ozone is {1}".format(particulates, ozone))
+        else:
+            self.speak_dialog("data could not be found")
 
     # Returns dict of data from the AirNow Api
-    def get_air_quality(self, lat, long) -> dict:
+    # Reuses Previous Data If Time Since Last Use Is Less Than 30 Min
+    def get_air_quality(self):
         # If Most Recent Usage Was Within 30 Minutes, Use That Data
         try:
             minutes = int(time.strftime('%M', time.localtime(os.path.getmtime("data.json"))))
@@ -35,37 +49,43 @@ class AirQualityIndex(MycroftSkill):
                     return json.load(file)
 
         # Perhaps There's No File Yet, Maybe It's Not A Valid Json Either Way, Get New Data
-        except:
-            pass
+        except Exception as err:
+            self.log.exception(f"Error Occurred: {err}")
 
-        PARAMS = {"latitude": lat,
-                  "longitude": long,
-                  "distance": distance,
-                  "API_KEY": api_key}
+        PARAMS = {"latitude": self.lat,
+                  "longitude": self.lon,
+                  "date": datetime.datetime.today().strftime('%Y-%m-%d'),  # Today
+                  "distance": 25,
+                  "API_KEY": self.api_key}
 
         try:
-            response = requests.get(url=URL, params=PARAMS, timeout=3).json()
+            response = requests.get(url=URL, params=PARAMS, timeout=3)
             response.raise_for_status()  # Raises HTTP Error If Something Went Wrong
 
         except requests.HTTPError as http_err:
-            self.log.exception(f"HTTP error occurred: {http_err}")  # Replace With Mycroft LOG
+            self.log.exception(f"HTTP error occurred: {http_err}")
+            return None
 
         except Exception as err:
-            self.log.exception(f"Other error occurred: {err}")  # Replace With Mycroft LOG
+            self.log.exception(f"Other error occurred: {err}")
+            return None
 
         else:
             try:
                 # Try To Save File To Reduce Potential API Calls
                 with open("data.json", "w") as file:
-                    file.write(json.dumps(response))
+                    file.write(json.dumps(response.json()))
 
             except Exception as err:
                 self.log.exception(f"JSON write error occurred: {err}")
                 pass
 
             # Returns The Dictionary
-            return json.load(response)
+            return response.json()
+
+    def stop(self):
+        pass
+
 
 def create_skill():
     return AirQualityIndex()
-

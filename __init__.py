acacee551c2430f87889587601226c99b333cc11
SamHarris2020 <sam.career@outlook.com>
2020-07-16 01:05:17 -0700

    simplified new version, added basic error handling

--- __init__.py
+++ __init__.py
@@ -1,9 +1,6 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
-import json
 import requests
-import os
-import time
 import datetime
 
 """
@@ -18,10 +15,10 @@ URL = "http://www.airnowapi.org/aq/forecast/latLong/?format=application/json"
 class AirQualityIndex(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+
         self.api_key = self.settings.get('api_key')
 
         loc = self.location
-
         self.lat = loc['coordinate']['latitude']
         self.lon = loc['coordinate']['longitude']
 
@@ -33,10 +30,12 @@ class AirQualityIndex(MycroftSkill):
         else:
             query = self.get_air_quality()
 
-            ozone = query[0]["Category"]["Name"]
-            particulates = query[1]["Category"]["Name"]
-            self.speak_dialog("particulates are {0} and ozone is {1}".format(particulates, ozone))
-
+            if query is not None:
+                ozone = query[0]["Category"]["Name"]
+                particulates = query[1]["Category"]["Name"]
+                self.speak_dialog("particulates are {0} and ozone is {1}".format(particulates, ozone))
+            else:
+                self.speak_dialog("something went wrong")
 
     def get_air_quality(self):
         PARAMS = {"latitude": self.lat,
@@ -45,57 +44,14 @@ class AirQualityIndex(MycroftSkill):
                   "distance": 25,
                   "API_KEY": self.api_key}
 
-        return requests.get(url=URL, params=PARAMS, timeout=3).json()
+        response = requests.get(url=URL, params=PARAMS, timeout=3)
 
-
-    # Returns dict of data from the AirNow Api
-    # Reuses Previous Data If Time Since Last Use Is Less Than 30 Min
-    # def get_air_quality(self):
-    #     # If Most Recent Usage Was Within 30 Minutes, Use That Data
-    #     try:
-    #         minutes = int(time.strftime('%M', time.localtime(os.path.getmtime("data.json"))))
-    #         if minutes < 60:
-    #             with open("data.json") as file:
-    #                 return json.load(file)
-    #
-    #     # Perhaps There's No File Yet, Maybe It's Not A Valid Json Either Way, Get New Data
-    #     except Exception as err:
-    #         self.log.exception(f"Error Occurred: {err}")
-    #
-    #     PARAMS = {"latitude": self.lat,
-    #               "longitude": self.lon,
-    #               "date": datetime.datetime.today().strftime('%Y-%m-%d'),  # Today
-    #               "distance": 25,
-    #               "API_KEY": self.api_key}
-    #
-    #     try:
-    #         self.log.debug(URL, PARAMS)
-    #         response = requests.get(url=URL, params=PARAMS, timeout=3)
-    #         response.raise_for_status()  # Raises HTTP Error If Something Went Wrong
-    #
-    #     except requests.HTTPError as http_err:
-    #         self.log.exception(f"HTTP error occurred: {http_err}")
-    #
-    #
-    #     except Exception as err:
-    #         self.log.exception(f"Other error occurred: {err}")
-    #
-    #     else:
-    #         try:
-    #             # Try To Save File To Reduce Potential API Calls
-    #             with open("data.json", "w") as file:
-    #                 file.write(json.dumps(response.json()))
-    #
-    #         except Exception as err:
-    #             self.log.exception(f"JSON write error occurred: {err}")
-    #             pass
-    #
-    #         # Returns The Dictionary
-    #         return response.json()
-
-    def stop(self):
-        pass
-
+        try:
+            response.raise_for_status()
+            return response.json()
+        except Exception as error:
+            self.log.debut(error)
+            return None
 
 def create_skill():
     return AirQualityIndex()
